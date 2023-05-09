051c5af303f35102b4071db81f0dafed7fabcde6
Mark Sheppard <mark_sheppard_1977@hotmail.com>
2023-05-09 10:21:25 +0100

    latest

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,59 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  List<Map<String, String>> info = [
+    {
+      "name": "Covid Safe",
+      "appIconUrl":
+          "https://api.madein.flutterbelgium.be/projects/Covid Safe/images/app_icon.webp"
+    },
+    {
+      "name": "Covid Scan",
+      "appIconUrl":
+          "https://api.madein.flutterbelgium.be/projects/Covid Scan/images/app_icon.webp"
+    },
+    {
+      "name": "EUDCC BE",
+      "appIconUrl":
+          "https://api.madein.flutterbelgium.be/projects/EUDCC BE/images/app_icon.webp"
+    },
+    {
+      "name": "Gaia",
+      "appIconUrl":
+          "https://api.madein.flutterbelgium.be/projects/Gaia/images/app_icon.webp"
+    },
+    {
+      "name": "Geluidshuis",
+      "appIconUrl":
+          "https://api.madein.flutterbelgium.be/projects/Geluidshuis/images/app_icon.webp"
+    },
+    {
+      "name": "GovApp",
+      "appIconUrl":
+          "https://api.madein.flutterbelgium.be/projects/GovApp/images/app_icon.webp"
+    },
+    {
+      "name": "Mijn Mooie Straat",
+      "appIconUrl":
+          "https://api.madein.flutterbelgium.be/projects/Mijn Mooie Straat/images/app_icon.webp"
+    },
+    {
+      "name": "Sibos",
+      "appIconUrl":
+          "https://api.madein.flutterbelgium.be/projects/Sibos/images/app_icon.webp"
+    },
+    {
+      "name": "m-Path",
+      "appIconUrl":
+          "https://api.madein.flutterbelgium.be/projects/m-Path/images/app_icon.webp"
+    },
+    {
+      "name": "nexuzhealth pro",
+      "appIconUrl":
+          "https://api.madein.flutterbelgium.be/projects/nexuzhealth pro/images/app_icon.webp"
+    }
+  ];
+
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -69,47 +122,62 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+    return LayoutBuilder(builder: (context, constraints) {
+      double screenWidth = constraints.maxWidth;
+      int iconCount = (screenWidth / 150).floor();
+      double iconWidth = screenWidth / iconCount;
+      return Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 100,
+          backgroundColor: Colors.grey.shade200,
+          centerTitle: false,
+          title: Text(widget.title),
+          actions: [
+            IconButton(
+                onPressed: null,
+                iconSize: 100,
+                icon: Image.network(
+                  'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
+                  width: 100,
+                  height: 100,
+                ))
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text('made in (Flutter) UK',
+                  style: TextStyle(fontSize: 72)),
+              const Text(
+                  'This collection of applications developed, published or contributed by Belgian developers or companies using the Flutter technology. This showcases the innovative and creative work of local developers and companies in the mobile/web app space.',
+                  style: TextStyle(fontSize: 24)),
+              Wrap(
+                children: info
+                    .map((e) => AnimatedContainer(
+                          duration: Duration(milliseconds: 50),
+                          width: iconWidth,
+                          child: MaterialButton(
+                            onPressed: () {},
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(24),
+                                child: Image.network(e['appIconUrl']!)),
+                            //iconSize: 160,
+                          ),
+                        ))
+                    .toList(),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      );
+    });
   }
 }
